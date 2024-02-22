6c3848131564a93b3b74e813ee457e16b1551ff7
sana.hsu <sana6622@gmail.com>
2024-02-22 17:17:35 +0800

    add .github\workflows

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? `/${process.env.REPOSITORY_NAME}/` : '/',
+  base: '/VuePractice-week6/',
   plugins: [
     vue(),
   ],
